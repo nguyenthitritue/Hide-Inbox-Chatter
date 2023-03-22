1f65e463e2484093507fa7d3ab7aa172c9adbaa7
nguyenthitritue <nguyenthitritue@gmail.com>
2023-03-22 11:08:15 +0700

    [Init]-Source

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Hide Inbox Chatter",
+    'license': 'AGPL-3',
+    'summary': """Hide Chatter and Inbox Hide Discussion Menu and chatter from Odoo""",
+    'description': """
+        Hide Chatter & Inbox
+        Hide Discussion Menu and chatter from Odoo Tray
+    """,
+    'version': "14.0",
+    'author': "odoo.solution.vn@gmail.com",
+    'support': 'odoo.solution.vn@gmail.com',
+    'images': ['static/description/img.png'],
+    'category': 'tools',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'qweb': [
+        'static/src/xml/template.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+}
