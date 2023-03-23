0a5a0923ce9eceb50920ba4b2d5d01820c7d0d20
nguyenthitritue <nguyenthitritue@gmail.com>
2023-03-23 10:50:12 +0700

    [Add] -Hide database manager

---
+++ hide_database_manager/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Hide database manager",
+    'license': 'AGPL-3',
+    'summary': """Hide link to database manager in login screen""",
+    'description': """
+        Hide link to database manager in login screen
+    """,
+    'version': "14.0",
+    'author': "odoo.solution.vn",
+    'support': 'odoo.solution.vn@gmail.com',
+    'images': ['static/description/img.png'],
+    'category': 'tools',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'data': [
+        'views/common_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+}
