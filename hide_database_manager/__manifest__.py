0b529a8c79cacdc7095d8f392cbdf8f78226d492
nguyenthitritue <nguyenthitritue@gmail.com>
2023-04-27 11:35:19 +0700

    [Add] -Hide database manager

--- hide_database_manager/__manifest__.py
+++ hide_database_manager/__manifest__.py
@@ -13,9 +13,8 @@
     'images': ['static/description/img.png'],
     'category': 'tools',
     'depends': ['base'],
-    'data': [
-
-    ],
+    'price': 4.99,
+    'currency': 'USD',
     'data': [
         'views/common_template.xml',
     ],
